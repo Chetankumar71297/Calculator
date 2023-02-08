c63895031faac8a29ac7b681397ebaaa04d61b5e
Chetan kumar <chetankumar71297@gmail.com>
2023-02-08 14:21:47 +0530

    Create basic maths function
    Created function for +,-,* and / and check then in browser console.

---
+++ script.js
@@ -0,0 +1,17 @@
+// Functions for all of the basic math operators
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
