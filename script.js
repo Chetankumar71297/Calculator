605fe985af82b996ff1681585d3d6de71363d35c
Chetan kumar <chetankumar71297@gmail.com>
2023-02-08 15:03:42 +0530

    Create operate function which gives result
    operate function takes numbers and operator as input and calls one of
    the basic maths function.Finally it returns the result of that operation

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2
 }
+
+// Function that takes an operator and 2 numbers as input and then calls one of the above functions on the numbers.
+
+function operate(operator, num1, num2) {
+    let result;
+    
+    if (operator === "+") {
+        result = add(num1, num2)
+    } else if (operator === "-") {
+        result = subtract(num1, num2)
+    } else if (operator === "*") {
+        result = multiply(num1, num2)
+    } else {
+        result = divide(num1, num2)
+    }
+
+    return result
+}
